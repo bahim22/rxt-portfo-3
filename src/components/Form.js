16adeb9edfbfbbb91ecbca86fd33f4336ff931a8
dionysus <dionysus@isitymarket.onmicrosoft.com>
2022-03-14 17:44:20 -0400

    possible formspree compos; will compare to other methods used prev & add;+ navbar compo edit

---
+++ src/components/Form.js
@@ -0,0 +1,29 @@
+// Make sure to run npm install @formspree/react
+// For more help visit https://formspr.ee/react-help
+import React from 'react'
+import { useForm, ValidationError } from '@formspree/react'
+
+
+export default function Form() {
+  const formKey = process.env.REACT_APP_FORM_ID
+  const [state, handleSubmit] = useForm({formKey})
+  if (state.succeeded) {
+    return <p>Thanks for joining!</p>
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor='email'>Email Address</label>
+      <input id='email' type='email' name='email' />
+      <ValidationError prefix='Email' field='email' errors={state.errors} />
+      <textarea id='message' name='message' />
+      <ValidationError prefix='Message' field='message' errors={state.errors} />
+      <button type='submit' disabled={state.submitting}>
+        Submit
+      </button>
+    </form>
+  )
+}
+// function App() {
+//   return <ContactForm />
+// }
+// export default App
