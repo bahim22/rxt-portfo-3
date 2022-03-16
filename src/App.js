e406f6577868f987e37abfd92fa81e196ecbc296
Hima Dionysus Balde <ibalde@isitymarket.onmicrosoft.com>
2022-03-16 16:37:53 -0400

    + icons; ran tasks and dev server; Todo: final edits - test => deploy

--- src/App.js
+++ src/App.js
@@ -2,26 +2,26 @@ import React from 'react'
 import About from './components/About'
 import './App.css'
 import Contact from './components/Contact'
-import Projects from './components/Projects';
-import Skills from './components/Skills';
-import Software from './components/Software';
-import Navbar from './components/Navbar';
+import Projects from './components/Projects'
+import Skills from './components/Skills'
+import Software from './components/Software'
+import Navbar from './components/Navbar'
 // import ContactForm from './components/ContactForm';
 // import { ButtonForm } from './components/ButtonForm';
 
 function App() {
-  return (
-    <main>
-      <Navbar />
-      <About />
-      <Projects />
-      <Skills />
-      <Software />
-      <Contact />
-      {/* <ContactForm  /> */}
-      {/* <ButtonForm  /> */}
-    </main>
-  )
+    return (
+        <main>
+            <Navbar />
+            <About />
+            <Projects />
+            <Skills />
+            <Software />
+            <Contact />
+            {/* <ContactForm  /> */}
+            {/* <ButtonForm  /> */}
+        </main>
+    )
 }
 
 export default App
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -3,29 +3,32 @@ import React from 'react'
 import { skills } from '../data'
 
 const Skills = () => {
-  return (
-    <section id='skills'>
-      <div className='container px-5 py-10 mx-auto'>
-        <div className='text-center mb-20'>
-          {/* <ChipIcon className='w-10 inline-block mb-4' /> */}
-          <h1 className='sm:text-4xl text-3xl font-medium title-font text-balck mb-4'>Skills &amp; Technologies</h1>
-          <p className='text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto'>
-            I'm skilled in <strong>Bash, Powershell, Command Line</strong> and am advancing my knowledge in Object-Oriented-Programming.
-          </p>
-        </div>
-        <div className='flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb- -mx-2'>
-          {skills.map((skill) => (
-            <div key={skill} className='p-2 sm:w-1/2 w-full'>
-              <div className='bg-gray-800 rounded flex p-4 h-full items-center'>
-                {/* <BadgeCheckIcon className='text-blue-300 w-6 h-6 flex-shrinl-0 mr-4' /> */}
-                <span className='title-font font-medium text-red-700'>{skill}</span>
-              </div>
+    return (
+        <section id='skills'>
+            <div className='container px-5 py-10 mx-auto'>
+                <div className='text-center mb-20'>
+                    {/* <ChipIcon className='w-10 inline-block mb-4' /> */}
+                    <h1 className='sm:text-4xl text-3xl font-medium title-font text-balck mb-4'>
+                        Skills &amp; Technologies
+                    </h1>
+                    <p className='text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto'>
+                        I`&apos;`m skilled in <strong>Bash, Powershell, Command Line</strong> and am advancing my
+                        knowledge in Object-Oriented-Programming.
+                    </p>
+                </div>
+                <div className='flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb- -mx-2'>
+                    {skills.map(skill => (
+                        <div key={skill} className='p-2 sm:w-1/2 w-full'>
+                            <div className='bg-gray-800 rounded flex p-4 h-full items-center'>
+                                {/* <BadgeCheckIcon className='text-blue-300 w-6 h-6 flex-shrinl-0 mr-4' /> */}
+                                <span className='title-font font-medium text-red-700'>{skill}</span>
+                            </div>
+                        </div>
+                    ))}
+                </div>
             </div>
-          ))}
-        </div>
-      </div>
-    </section>
-  )
+        </section>
+    )
 }
 
 export default Skills
