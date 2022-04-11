2ddc42eff5d04b7968ef8161d4a826a45d614add
Hima Dionysus Balde <ibalde@isitymarket.onmicrosoft.com>
2022-04-10 21:26:31 -0400

    added assets

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -19,7 +19,7 @@ const Projects = () => {
 				</div>
 				<div className='flex flex-wrap -m-4'>
 					{ProjectList.map((project) => (
-						<a href={project.link} key={project.image} className='sm:w-1/2 w-100 p-4'>
+						<a href={project.link} key={project.id} className='sm:w-1/2 w-100 p-4'>
 							<div className='flex relative'>
 								<img
 									src={project.image}
