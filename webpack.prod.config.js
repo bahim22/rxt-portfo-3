2c77004bde7a60837448c5eec288d7ccde0f4542
Hima Dionysus Balde <ibalde@isitymarket.onmicrosoft.com>
2022-04-14 03:45:18 -0400

    small adjust: jsconfig and webpack prod

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -148,10 +148,5 @@ module.exports = {
 	},
 	resolve: {
 		extensions: ['.js', '.jsx'],
-		modules: [
-			path.resolve(__dirname, 'src'),
-			path.resolve(__dirname, 'node_modules'),
-			path.resolve(__dirname, 'public'),
-		],
 	},
 };
