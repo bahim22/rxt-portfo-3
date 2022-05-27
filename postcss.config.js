a26dc0156dcbe5dfa4f84652c63544bebae47aa8
Hima Dionysus Balde <ibalde@isitymarket.onmicrosoft.com>
2022-05-26 22:41:28 -0400

    rm extra files + fix headers

--- postcss.config.js
+++ postcss.config.js
@@ -1,51 +1,12 @@
-// 	// Add you postcss configuration here
-
 // const { default: postcss } = require( "postcss" )
 
-// 	// Learn more about it at https://github.com/webpack-contrib/postcss-loader#config-files
 module.exports = {
-	plugins: [['autoprefixer', 'tailwindcss', 'postcss-preset-env']],
-}
-module.exports = {
-	plugins: {
-		'tailwindcss': {},
-		'autoprefixer': {},
-        'postcss-preset-env': {},
-	},
+    plugins: [['autoprefixer', 'tailwindcss', 'postcss-preset-env']],
 };
-
-// const postcssPresetEnv = require('postcss-preset-env');
-
-// module.exports = {
-//     plugins: [
-//         postcssPresetEnv({
-//             browsers: ['>0.25%', 'not ie 11', 'not op_mini all']
-//         }),
-//         require('cssnano')
-//     ]
-// }
-/* const tailwindcss = require('tailwindcss');
-// const purgecss = require('@fullhuman/postcss-purgecss');
-const autoprefixer = require('autoprefixer');
-const postcsspresetenv = require('postcss-preset-env');
-
 module.exports = {
     plugins: {
-        tailwindcss: { tailwindcss },
-        autoprefixer: { autoprefixer },
-        postcsspresetenv: { postcsspresetenv },
+        tailwindcss: {},
+        autoprefixer: {},
+        'postcss-preset-env': {},
     },
 };
-// ? formspree root info
-{
-  "forms": {
-    "contactForm": {
-      "name": "Contact Form",
-      "actions": [{
-        "type": "email",
-        "to": "email@gmail.com"
-      }]
-    }
-  }
-}
- */
