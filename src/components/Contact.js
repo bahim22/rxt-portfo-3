fef8d0573ffa2213a0973475f315acc49dab046f
Hima Dionysus Balde <ibalde@isitymarket.onmicrosoft.com>
2022-06-12 03:47:07 -0400

    edit forms and css in contact compo

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,25 +7,26 @@ const Contact = () => {
     return (
         <section id='contact'>
             <Box sx={{ display: 'flex', flexWrap: 'wrap', alignItems: 'center', justifyContent: 'space-between' }}>
-                <Box sx={{ mr: 2, display: 'flex', alignItems: 'center' }}>
-                    <Avatar
-                        alt='Hima Avatar'
-                        title='Hima Avatar'
-                        src='https://readyplayer.me/gallery/Fin2iIPGx'
-                        sx={{ width: 34, height: 34, marginRight: 2.75 }}
-                    />
-                    <Typography variant='body2' sx={{ color: 'common.white' }}>
-                        Anne Burke
-                    </Typography>
-                    <Typography>
-                        <Link href='https://forms.office.com/Pages/AnalysisPage.aspx?AnalyzerToken=pmJOsezZXc5CYi8EhyrHfQ0mljOJhK6e&id=R_nkmWwVKU2oUbLcTQ9XnngdfUa9-rlAmSxh5KMiCLJUNUZINUpQQ0tKVTY2QldGS1ZBOExLM0ZGUS4u'>
-                            <strong>Feedback Form</strong>
-                        </Link>
-                    </Typography>
-                </Box>
+                <Avatar
+                    alt='Hima Avatar'
+                    title='Hima Avatar'
+                    src='/hima2.jpeg'
+                    // src='/gravatar.png'
+                    sx={{ width: 34, height: 34, marginRight: 2.75 }}
+                />
+            </Box>
+            <Box sx={{ mr: 2, display: 'flex', alignItems: 'center' }}>
+                <Typography variant='body2' sx={{ color: 'common.white' }}>
+                    Hima Avatar
+                </Typography>
+                <Typography>
+                    <Link href='https://forms.office.com/r/16Fh6afNmV'>
+                        <strong>Microsoft 365 Feedback Form</strong>
+                    </Link>
+                </Typography>
             </Box>
             <div className='container px-5 py-10 mx-auto flex sm:flex-wrap flex-wrap'>
-                <div className='sm:min-w-fit lg:min-w-full bg-gray-400 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-center justify-around fixed shadow-md shadow-teal-400'>
+                <div className='sm:min-w-full lg:min-w-full bg-gray-400 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-center justify-around shadow-md shadow-teal-400'>
                     <iframe
                         width='100%'
                         height='300'
@@ -45,20 +46,22 @@ const Contact = () => {
                         <p className='mt-1'>Pittsburgh, PA 15219</p>
                     </div>
                     <div className='lg:w-1/2 px-6 lg:mt-0'>
-                        <h2 className='title-font font-semibold text-white tracking-widest text-sm'>Email: </h2>
+                        <h4 className='title-font font-semibold text-white tracking-widest text-sm'>Email: </h4>
                         <Link
                             href='mailto:ibalde@dionysuseradev.onmicrosoft.com'
                             className='text-slate-500 leading-relaxed'>
-                            <h2 className='title-font font-semibold text-white tracking-widest text-sm'>
-                                <strong>Hima</strong>
-                            </h2>
+                            <strong>Hima</strong>
                         </Link>
+                        <h5 className='title-font font-semibold text-white tracking-widest text-sm'>
+                            Send me an Email
+                        </h5>
+                        <h5 className='title-font font-semibold text-white tracking-widest text-sm'>
+                            <strong>Google Business Page</strong>
+                        </h5>
                         <Link
                             href='https://g.page/r/CXfswPHp0DDEEAE/review'
                             className='text-slate-500 leading-relaxed '>
-                            <h2 className='title-font font-semibold text-white tracking-widest text-sm'>
-                                <strong>Google Review</strong>
-                            </h2>
+                            <strong>Google Review</strong>
                         </Link>
                     </div>
                 </div>
